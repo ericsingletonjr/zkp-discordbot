202edb313ddb0a727c3d5b2bbcb3acc920b84075
Eric Singleton <eric.singleton.jr@gmail.com>
2019-07-21 17:36:51 -0700

    Added google spreadsheets api consumption

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-import discord
+#=== IMPORT SECTION ===
 import os
+import json
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import discord
+
+#=== CLIENT SETUPS ===
 token = os.environ.get('TOKEN')
 
-client = discord.Client()
+gData = {
+    "type": os.environ.get('type'),
+    "project_id": os.environ.get('project_id'),
+    "private_key_id": os.environ.get('private_key_id'),
+    "private_key": os.environ.get('private_key'),
+    "client_email": os.environ.get('client_email'),
+    "client_id": os.environ.get('client_id'),
+    "auth_uri": os.environ.get('auth_uri'),
+    "token_uri": os.environ.get('token_uri'),
+    "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
+    "client_x509_cert_url": os.environ.get('client_x509_cert_url')
+}
 
-@client.event
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_dict(gData, scope)
+gClient = gspread.authorize(creds)
+dClient = discord.Client()
+
+#test connection
+sheet = gClient.open('Deja Vu').sheet1
+sheet2 = gClient.open('Deja Vu Backend').sheet1
+list_of_hashes = sheet.get_all_records()
+print(sheet.row_values(2))
+sheet.update_cell(2,3, "connected from heroku")
+print("-------")
+
+#=== MAIN PROGRAM ===
+@dClient.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == dClient.user:
         return
     
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
 
-@client.event
+@dClient.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(dClient.user.name)
+    print(dClient.user.id)
     print('------')
-    await client.change_presence(activity=discord.Game(name="broken af right"))
+    await dClient.change_presence(activity=discord.Game(name="broken af right"))
 
-client.run(token)
+dClient.run(token)
