c55209ecce0445f32634b16820f5ecd0958eefcf
Zuhayr <ballsupinc@gmail.com>
2019-08-03 15:35:41 -0700

    change variable location from backend to variable sheet in main

--- dkp_var_config.py
+++ dkp_var_config.py
@@ -7,13 +7,14 @@ class DKP_Vars:
     def __init__(self, gClient):
         self.gClient = gClient
         print("=== SETTING UP DKP VARIABLES ===")
-        DKPSheet = gClient.open("Deja Vu Backend").sheet1
-        self.Tithe = DKPSheet.acell("A2").value
-        self.AttendencePay = DKPSheet.acell("B2").value
-        self.MinBid = DKPSheet.acell("C2").value
-        self.PerPlayerAmount = DKPSheet.acell("A5").value
-        self.DKPScale = DKPSheet.acell("B5").value
-        self.Bankcut = DKPSheet.acell("C5").value
-        self.MaxTip = DKPSheet.acell("B8").value
-        self.TipLimitPerWeek = DKPSheet.acell("C8").value
-        self.TrialPayout = DKPSheet.acell("A10").value
+        DKPSheet = gClient.open("Deja Vu").worksheet("Variables")
+        self.Tithe = DKPSheet.acell("B2").value
+        self.AttendencePay = DKPSheet.acell("B3").value
+        self.Bankcut = DKPSheet.acell("B4").value
+        self.StalePenalty = DKPSheet.acell("B5").value
+        self.MinBid = DKPSheet.acell("B6").value
+        self.PerPlayerAmount = DKPSheet.acell("B7").value
+        self.BankStartDKP = DKPSheet.acell("B8").value
+        self.MaxTip = DKPSheet.acell("B9").value
+        self.TipLimitPerWeek = DKPSheet.acell("B10").value
+        self.TrialPayout = DKPSheet.acell("B11").value
